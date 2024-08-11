8377335611cc89e5b6097aee9d391bc76e03b346
Sharvani Vadlamani <sv2734@columbia.edu>
2024-08-10 22:09:50 -0400

    fixed the config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql://shar:carrots123@localhost/crypto_db' #PUT Your OWN username and password    SECRET_KEY = '850b25db06cfa9ae390c26c23ec6daa8' #PUT Your Own Secret Key
+    SECRET_KEY = '850b25db06cfa9ae390c26c23ec6daa8' #PUT Your Own Secret Key
     SQLALCHEMY_TRACK_MODIFICATIONS=True
